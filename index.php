57cfa8909ae988403039983f490dc9a6500010c8
Ahiram <a.hiram.art@hotmail.com>
2026-02-16 11:49:02 -0600

    Se añadió linea de codigo para añadir diseño responsivo usando Bootstrap

--- index.php
+++ index.php
@@ -33,7 +33,11 @@ if (!isset($_SESSION["estudiantes"])) {
 <!-- head -->
   <head>
     <meta charset="UTF-8">
+    <!-- responsive degign -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <title>Estudiantes</title>
+    
     <link 
 href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" 
       rel="stylesheet">
